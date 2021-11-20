a1139d199a14b119df5a849a4ebc8512aaa4ab52
divit-agrawal <73773075+divit-agrawal@users.noreply.github.com>
2021-11-21 01:30:16 +0530

    feat: furnitureData route

--- routes/furnitureData.js
+++ routes/furnitureData.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const { promisify } = require("util");
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 const fs = require("fs");
 
@@ -10,7 +9,7 @@ const CREDENTIALS = JSON.parse(
   fs.readFileSync("furnitar-332710-646f576c7040.json")
 );
 
-const getRow = async (l,b,h,type) => {
+const getRow = async (l, b, h, type) => {
   await doc.useServiceAccountAuth({
     client_email: CREDENTIALS.client_email,
     private_key: CREDENTIALS.private_key,
@@ -18,10 +17,16 @@ const getRow = async (l,b,h,type) => {
   await doc.loadInfo();
   let sheet = doc.sheetsByIndex[0];
   let rows = await sheet.getRows();
+  var arr = [];
   for (let index = 0; index < rows.length; index++) {
     const row = rows[index];
-    if(row.product_type === type && row.length <= l && row.breadth <= b && row.height <= h ){
-      console.log('---------------');
+    if (
+      row.product_type === type &&
+      row.length <= l &&
+      row.breadth <= b &&
+      row.height <= h
+    ) {
+      console.log("---------------");
       console.log(row.product_name);
       console.log(row.length);
       console.log(row.breadth);
@@ -30,17 +35,34 @@ const getRow = async (l,b,h,type) => {
       console.log(row.product_selection1);
       console.log(row.product_selection2);
       console.log(row.product_type);
-      console.log('---------------');
+      console.log("---------------");
+      const obj = {
+        length: row.length,
+        width: row.breadth,
+        height: row.height,
+        product_name: row.product_name,
+        product_url: row.product_url,
+        price: row.product_selection1,
+        image_url: row.product_selection2,
+      };
+      arr.push(obj);
     }
-   
   }
+  return arr;
 };
 
-getRow(101.6,48.26,76.2,'table');
-
-//to do
 router.post("/", async (req, res) => {
-  res.send({ message: res });
+  let length = req.body.length,
+    width = req.body.width,
+    height = 5000;
+  console.log(req.body.length);
+  if (!req.body.length && !req.body.width && !req.body.category) {
+    res.send({ message: "Incomplete params" });
+    return;
+  }
+  if (req.body.height != null) height = req.body.height;
+  let ar = await getRow(length, width, height, req.body.category);
+  res.status(200).send(ar);
 });
 
 module.exports = router;
