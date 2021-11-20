4d5bcf3399ff8e42cd216e5459063c7e586d0793
rishimalgwa <rishimalgwa35@gmail.com>
2021-11-21 00:40:09 +0530

    query added

--- routes/furnitureData.js
+++ routes/furnitureData.js
@@ -10,19 +10,33 @@ const CREDENTIALS = JSON.parse(
   fs.readFileSync("furnitar-332710-646f576c7040.json")
 );
 
-const getRow = async () => {
+const getRow = async (l,b,h,type) => {
   await doc.useServiceAccountAuth({
     client_email: CREDENTIALS.client_email,
     private_key: CREDENTIALS.private_key,
   });
   await doc.loadInfo();
   let sheet = doc.sheetsByIndex[0];
-  let rows = await sheet.getRows().then((data) => {
-    console.log(data);
-  });
+  let rows = await sheet.getRows();
+  for (let index = 0; index < rows.length; index++) {
+    const row = rows[index];
+    if(row.product_type === type && row.length <= l && row.breadth <= b && row.height <= h ){
+      console.log('---------------');
+      console.log(row.product_name);
+      console.log(row.length);
+      console.log(row.breadth);
+      console.log(row.height);
+      console.log(row.product_url);
+      console.log(row.product_selection1);
+      console.log(row.product_selection2);
+      console.log(row.product_type);
+      console.log('---------------');
+    }
+   
+  }
 };
 
-getRow();
+getRow(101.6,48.26,76.2,'table');
 
 //to do
 router.post("/", async (req, res) => {
