c0f5a4f9a764a21afd4d9e1b1f64b4d14f94f0b6
rishimalgwa <rishimalgwa35@gmail.com>
2021-11-20 17:09:50 +0530

    google sheet added

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 const express = require("express");
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const fs = require('fs')
+
+
+const RESPONSE_SHEET_ID = '1Bo-syLS0csDbqM-fdh66rWgrljnvS6pxJRd2kN_ht-s';
+const doc = new GoogleSpreadsheet(RESPONSE_SHEET_ID);
+const CREDENTIALS = JSON.parse(fs.readFileSync('furnitar-332710-646f576c7040.json'))
+
+const getRow = async ()=>{
+
+  await doc.useServiceAccountAuth({
+    client_email: CREDENTIALS.client_email,
+    private_key: CREDENTIALS.private_key,
+  });
+  await doc.loadInfo();
+  let sheet = doc.sheetsByIndex[0];
+  let rows = await sheet.getRows();
+  console.log(rows);
+}
+
+
 
 const app = express();
 
@@ -7,7 +28,7 @@ const PORT = 3000;
 const bodyParser = require("body-parser");
 
 const cors = require("cors");
-
+getRow();
 const furnitureData = require("./routes/furnitureData");
 app.use(
   bodyParser.urlencoded({
